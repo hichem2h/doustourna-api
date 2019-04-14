ec56a4cd581c2cb46314ae195bf1b94f099d827d
hichem2h <hichem2h@gmail.com>
2019-04-14 02:04:54 +0100

    Add cors

--- doustourna/settings/base.py
+++ doustourna/settings/base.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'api',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+CORS_ORIGIN_ALLOW_ALL = True
